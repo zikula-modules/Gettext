5f49e10b945716deaa2c79eef26f534db2bc7483
Craig Heydenburg <craigh@mac.com>
2014-03-10 16:58:45 -0400

    various controller refactorings

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -12,71 +12,70 @@ namespace Zikula\GettextModule\Controller;
 
 use LogUtil;
 use SecurityUtil;
-use Zikula_View;
-use ZLanguage;
-use FormUtil;
 use ModUtil;
 use System;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 class UserController extends \Zikula_AbstractController
 {
     /**
      * entry point for the module
      */
-    public function main($args)
+    public function mainAction()
     {
         // security check
         if (!SecurityUtil::checkPermission('Gettext::', '::', ACCESS_READ)) {
             return LogUtil::registerPermissionError();
         }
         // create a new output object
-        $view = Zikula_View::getInstance('Gettext', false);
-        $view->assign('mtype', '');
-        return $view->fetch('gettext_user_extract.htm');
+        $this->view->setCaching(false);
+        $this->view->assign('mtype', '');
+        return $this->response($this->view->fetch('User/extract.tpl'));
     }
     
-    public function extract($args)
+    public function extractAction()
     {
         // security check
         if (!SecurityUtil::checkPermission('Gettext::', '::', ACCESS_READ)) {
             return LogUtil::registerPermissionError();
         }
-        $thisdomain = ZLanguage::getModuleDomain('Gettext');
-        $view = Zikula_View::getInstance('Gettext', false);
-        $component = FormUtil::getPassedValue('component', '', 'POST');
+        $this->view->setCaching(false);
+        $component = $this->request->request->get('component', '');
         $component = preg_replace('/([^a-zA-Z0-9|^\\-|^_])/', '', $component);
-        $mtype = FormUtil::getPassedValue('mtype', '', 'POST');
+        $mtype = $this->request->request->get('mtype', '');
+        $this->view->assign('mtype', $mtype);
         $domain = strtolower("{$mtype}_{$component}");
-        $submit = FormUtil::getPassedValue('submit', null, 'POST');
+        $submit = $this->request->request->get('submit', null);
         if (!isset($submit)) {
-            return $view->fetch('gettext_user_extract.htm');
+            $this->view->assign('mtype', '');
+            return $this->response($this->view->fetch('User/extract.tpl'));
         }
         if (!$mtype) {
-            LogUtil::registerError(__('You must select module or theme', $thisdomain));
-            return $view->fetch('gettext_user_extract.htm');
+            $this->request->getSession()->getFlashBag()->add('error', $this->__('You must select module or theme'));
+            return $this->response($this->view->fetch('User/extract.tpl'));
         }
         if (!$component) {
-            LogUtil::registerError(__('You must enter the name of the module or theme (case sensitive)', $thisdomain));
-            return $view->fetch('gettext_user_extract.htm');
+            $this->request->getSession()->getFlashBag()->add('error', $this->__('You must enter the name of the module or theme (case sensitive)'));
+            return $this->response($this->view->fetch('User/extract.tpl'));
         }
         if (empty($_FILES['archive']['tmp_name'])) {
-            LogUtil::registerError(__('Please specify zip/tgz file!', $thisdomain));
-            return $view->fetch('gettext_user_extract.htm');
+            $this->request->getSession()->getFlashBag()->add('error', $this->__('Please specify zip/tgz file!'));
+            return $this->response($this->view->fetch('User/extract.tpl'));
         }
-        // get files
-        $archive = $_FILES['archive']['tmp_name'];
+
         // setup
         $time = microtime();
         $pid = sha1($time);
         $path = "/tmp/{$pid}";
         // get files
         $archive = $_FILES['archive']['tmp_name'];
-        $helper = file_get_contents('modules/Gettext/helpers/xtractor.sh');
+        $helper = file_get_contents('modules/Gettext/Helper/xtractor.sh');
         file_put_contents('/tmp/xtractor.sh', $helper);
         `chmod 755 /tmp/xtractor.sh`;
-        $helper = file_get_contents('modules/Gettext/helpers/xcompile.php');
+        $helper = file_get_contents('modules/Gettext/Helper/xcompile.php');
         file_put_contents('/tmp/xcompile.php', $helper);
-        $helper = file_get_contents('modules/Gettext/helpers/xcompilejs.php');
+        $helper = file_get_contents('modules/Gettext/Helper/xcompilejs.php');
         file_put_contents('/tmp/xcompilejs.php', $helper);
         $command = "/tmp/xtractor.sh {$path} {$archive} {$component} {$domain} zip none";
         $output = '';
@@ -88,31 +87,31 @@ class UserController extends \Zikula_AbstractController
         `/bin/rm -f /tmp/xcompile.php`;
         `/bin/rm -f /tmp/xcompilejs.php`;
         `rm -rf /{$path}/{$component}`;
-        $view->assign('result', $result);
-        $view->assign('key', $pid);
-        $view->assign('c', $component);
-        $view->assign('d', $domain);
-        $view->assign('output', $output);
-        return $view->fetch('gettext_user_download.htm');
+        $this->view->assign('result', $result);
+        $this->view->assign('key', $pid);
+        $this->view->assign('c', $component);
+        $this->view->assign('d', $domain);
+        $this->view->assign('output', $output);
+        return $this->response($this->view->fetch('User/download.tpl'));
     }
     
-    public function download($args)
+    public function downloadAction()
     {
         // security check
         if (!SecurityUtil::checkPermission('Gettext::', '::', ACCESS_READ)) {
             return LogUtil::registerPermissionError();
         }
-        $key = FormUtil::getPassedValue('key', null, 'GET');
+        $key = $this->request->query->get('key', null);
         $key = preg_replace('/([^a-zA-Z0-9|^\\-|^_])/', '', $key);
-        $c = FormUtil::getPassedValue('c', null, 'GET');
+        $c = $this->request->query->get('c', null);
         $c = preg_replace('/([^a-zA-Z0-9|^\\-|^_])/', '', $c);
-        $d = FormUtil::getPassedValue('d', null, 'GET');
+        $d = $this->request->query->get('d', null);
         $d = preg_replace('/([^a-zA-Z0-9|^\\-|^_])/', '', $d);
         $file = "/tmp/{$key}/{$d}.zip";
         $contents = file_get_contents($file);
         $length = filesize($file);
         if ($length < 1) {
-            System::redirect(ModUtil::url('Gettext'));
+            System::redirect(ModUtil::url('Gettext', 'user', 'main'));
         }
         ob_end_clean();
         ini_set('zlib.output_compression', 0);
@@ -126,27 +125,26 @@ class UserController extends \Zikula_AbstractController
         die;
     }
     
-    public function compilemo()
+    public function compilemoAction()
     {
-        $domain = ZLanguage::getModuleDomain('Gettext');
-        $view = Zikula_View::getInstance('Gettext', false);
+        $this->view->setCaching(false);
         // security check
         if (!SecurityUtil::checkPermission('Gettext::', '::', ACCESS_READ)) {
             return LogUtil::registerPermissionError();
         }
-        $submit = FormUtil::getPassedValue('submit', null, 'POST');
-        $forcefuzzy = FormUtil::getPassedValue('forcefuzzy', false, 'POST');
+        $submit = $this->request->request->get('submit', null);
+        $forcefuzzy = $this->request->request->get('forcefuzzy', false);
         $forcefuzzy = $forcefuzzy ? 'yes' : 'no';
         if (!isset($submit)) {
-            return $view->fetch('gettext_user_compilemo.htm');
+            return $this->response($this->view->fetch('User/compilemo.tpl'));
         }
         if (empty($_FILES['po']['tmp_name'])) {
-            LogUtil::registerError(__('Please specify .po file!', $domain));
-            return $view->fetch('gettext_user_compilemo.htm');
+            $this->request->getSession()->getFlashBag()->add('error', $this->__('Please specify .po file!'));
+            return $this->response($this->view->fetch('User/compilemo.tpl'));
         }
         // get files
         $po = $_FILES['po']['tmp_name'];
-        $helper = file_get_contents('modules/Gettext/helpers/xcompilemo.sh');
+        $helper = file_get_contents('modules/Gettext/Helper/xcompilemo.sh');
         file_put_contents('/tmp/xcompilemo.sh', $helper);
         `chmod 755 /tmp/xcompilemo.sh`;
         // setup
@@ -155,23 +153,23 @@ class UserController extends \Zikula_AbstractController
         $path = "/tmp/{$pid}";
         `/tmp/xcompilemo.sh {$path} {$po} {$forcefuzzy}`;
         `rm -f /tmp/xcompilemo.sh`;
-        $view->assign('key', $pid);
-        return $view->fetch('gettext_user_downloadmo.htm');
+        $this->view->assign('key', $pid);
+        return $this->response($this->view->fetch('User/downloadmo.tpl'));
     }
     
-    public function downloadmo($args)
+    public function downloadmoAction()
     {
         // security check
         if (!SecurityUtil::checkPermission('Gettext::', '::', ACCESS_READ)) {
             return LogUtil::registerPermissionError();
         }
-        $key = FormUtil::getPassedValue('key', null, 'GET');
+        $key = $this->request->query->get('key', null);
         $key = preg_replace('/([^a-zA-Z0-9|^\\-|^_])/', '', $key);
         $file = "/tmp/{$key}/messages.mo";
         $contents = file_get_contents($file);
         $length = strlen($contents);
         if ($length < 1) {
-            System::redirect(ModUtil::url('Gettext'));
+            return new RedirectResponse(System::normalizeUrl(ModUtil::url('Gettext', 'user', 'main')));
         }
         header('Content-Type: application/octet-stream');
         header("Content-Length: {$length}");
